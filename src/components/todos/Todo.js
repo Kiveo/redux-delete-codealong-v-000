5f041f26a294d19bcf072a13a243170e052d2283
Maverick Tesini <mavericktesini@gmail.com>
2018-09-22 03:28:32 +0000

    automatically backed up by learn

--- src/components/todos/Todo.js
+++ src/components/todos/Todo.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const Todo = props => {
-  <div>
-    <span>{props.text}</span><button>DELETE</button>
-  </div>
+  return (
+    <div>
+      <span>{props.text}</span><button>DELETE</button>
+    </div>
+  )
 }
 
 export default Todo;
